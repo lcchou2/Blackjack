c7e1e7fdf12ff900cd2ee39532aad3ffc4e7277f
Lawrence Chou <lawrencechou@Lawrences-MacBook-Pro.local>
2019-08-13 16:19:53 -0700

    No direct mutation of state in any functions, need to re-implement double, Ace functionality, coloring, and minor css bugs

--- blackjack/src/App.js
+++ blackjack/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 
@@ -31,47 +31,99 @@ class App extends React.Component {
     this.dealCards = this.dealCards.bind(this)
     this.generateCount = this.generateCount.bind(this)
     this.playerHit = this.playerHit.bind(this)
-    this.playStand = this.playStand.bind(this)
+    // this.playStand = this.playStand.bind(this)
     this.checkBJ = this.checkBJ.bind(this)
     this.win = this.win.bind(this)
     this.lose = this.lose.bind(this)
     this.bjwin = this.bjwin.bind(this)
     this.draw = this.draw.bind(this)
     this.double = this.double.bind(this)
+    this.checkCount = this.checkCount.bind(this)
+    this.dealerHit = this.dealerHit.bind(this)
+  }
+  dealerHit() {
+    const dealo = this.state.dealer.count 
+    const counto = this.state.player.count
+    if (dealo < 17) {
+      const cardo = this.generateCard(this.state.deck)
+      const newDealer = this.state.dealer.cards
+
+    
+    newDealer.push(cardo.randomCard)
+
+    this.setState(prevState => {
+      let dealer = Object.assign({}, prevState.dealer);  // creating copy of state variable jasper
+      dealer.count = dealo + this.generateCount(cardo.randomCard)
+      dealer.cards = newDealer                    // update the name property, assign a new value                 
+      return { dealer , deck:cardo.newDeck};                                 // return new object jasper object
+    },()=>{
+      this.checkCount()
+      this.dealerHit()
+    })
+      
+
+    } else {
+     
+      if (counto > dealo || dealo > 21) {
+        this.win()
+      } else if (counto === dealo){ 
+        this.draw()
+      }else {
+        this.lose()
+      }
+    }
+    
+     
+  }
+
+  // playStand(){
+  //   if (this.state.dealer.count < 17) {
+  //     const cardo = this.generateCard(this.state.deck)
+  //     const newDealer = this.state.dealer.cards
+  //     newDealer.push(cardo.randomCard)
+  //     this.state.dealer.count = this.state.dealer.count + this.generateCount(cardo.randomCard)
+  //     this.state.dealer.cards = newDealer
+  //     this.setState({})
+  //     this.playStand()
+      
+  //   } else {
+     
+  //     if (this.state.player.count > this.state.dealer.count || this.state.dealer.count > 21) {
+  //       this.win()
+  //     } else if (this.state.player.count === this.state.dealer.count){ 
+  //       this.draw()
+  //     }else {
+  //       this.lose()
+  //     }
+  //   }
+    
+  // }
+
+  checkCount() {
+    
+    
+    const counto = this.state.player.count
+    const dealo = this.state.dealer.count
+    if (counto > 21 ) {
+      this.lose()
+    } else if (dealo > 21) {
+      this.win()
+    }
   }
 
   double() {
-    this.setState({
-      bet: this.state.bet * 2
-    })
-    const cardo = this.generateCard(this.state.deck)
-    const newPlayer = this.state.player.cards
-  
-  newPlayer.push(cardo.randomCard)
-  
-  this.state.player.count = this.state.player.count + this.generateCount(cardo.randomCard)
-  this.state.player.cards = newPlayer
-  if (this.state.player.count > 21) {
-    this.setState({
-      message: "you LOSE ",
-      cash: this.state.cash - this.state.bet*2,
-      betTemp:0
-    })
-    setTimeout(() => {
-      this.softReset()
-     
-    }, 2000)
-  } else {
-    this.setState({
-      bet: this.state.bet * 2
-    })
-    this.playStand()
+    this.playerHit()
+
+    if (this.state.betTemp !== 0) {
+      this.playStand()
+    } 
+    
   }
 
   
 
     
-  }
+  
   draw(){
     this.setState({
       message: "you Draww",
@@ -172,8 +224,8 @@ class App extends React.Component {
     const dealerCard1 = this.generateCard(playerCard1.newDeck);
     const playerCard2 = this.generateCard(dealerCard1.newDeck); 
     const dealerCard2 = this.generateCard(playerCard2.newDeck)   
-    const player = {cards: [playerCard1.randomCard, playerCard2.randomCard], count: this.generateCount(playerCard1.randomCard) + this.generateCount(playerCard2.randomCard)}
-    const dealer = {cards: [dealerCard1.randomCard, dealerCard2.randomCard], count: this.generateCount(dealerCard1.randomCard) + this.generateCount(dealerCard2.randomCard)}
+    const player = {cards: [playerCard1.randomCard, playerCard2.randomCard], count: this.generateCount(playerCard1.randomCard) + this.generateCount(playerCard2.randomCard), altcount: ""}
+    const dealer = {cards: [dealerCard1.randomCard, dealerCard2.randomCard], count: this.generateCount(dealerCard1.randomCard) + this.generateCount(dealerCard2.randomCard), altcount: ""}
   
     
     return {newDeck :dealerCard2.newDeck, player, dealer}
@@ -229,85 +281,73 @@ class App extends React.Component {
   
 
   start() {
+   
+    console.log(this.state.val)
     
     if (this.state.val > this.state.cash){
       this.setState({
         message: "nope nibba",
         bet: "0"
       })
-    } else if (this.state.bet === 0 && this.state.val === "") {
-      alert("You gotta bet something")
-    } else if (typeof this.state.val === 'number'){
-      
+    }  else if (typeof this.state.val === 'number'){
+      console.log(this.state.betTemp)
       this.setState({
         bet: this.state.val,
         hand:"block",
-        betTemp: this.state.bet
+        betTemp: this.state.val
       
-      })
+      }, this.checkBJ())
 
-      this.checkBJ();
-    }else {
+      
+    }else if (this.state.bet === 0 && this.state.val === "") {
+      alert("You gotta bet something")
+    }
+    
+    else {
       
       this.setState({
         hand:"block",
-        betTemp: this.state.bet
+        betTemp: this.state.val
       
-      })
+      },this.checkBJ())
 
-      this.checkBJ();
+      
     }
     
   }
 
   playerHit() {
-    
+  
     if (this.state.betTemp === 0) {
       alert("bet something first")
     } else {
       const cardo = this.generateCard(this.state.deck)
       const newPlayer = this.state.player.cards
+      const counto = this.state.player.count
     
     newPlayer.push(cardo.randomCard)
+
+    this.setState(prevState => {
+      let player = Object.assign({}, prevState.player);  // creating copy of state variable jasper
+      player.count = counto + this.generateCount(cardo.randomCard)
+      player.cards = newPlayer                    // update the name property, assign a new value                 
+      return { player , deck:cardo.newDeck, betTemp: this.state.bet};                                 // return new object jasper object
+    },this.checkCount)
+
     
-    this.state.player.count = this.state.player.count + this.generateCount(cardo.randomCard)
-    this.state.player.cards = newPlayer
+
+    // this.setState({
+    //   player:{count:counto + this.generateCount(cardo.randomCard), cards: newPlayer}
+    // })
     
-      
-      if (this.state.player.count > 21) {
-        this.lose()
-      }
-      this.setState({
-        deck: cardo.newDeck
-      })
+    
 
     }
 
     
   }
 
-  playStand(){
-    if (this.state.dealer.count < 17) {
-      const cardo = this.generateCard(this.state.deck)
-      const newDealer = this.state.dealer.cards
-      newDealer.push(cardo.randomCard)
-      this.state.dealer.count = this.state.dealer.count + this.generateCount(cardo.randomCard)
-      this.state.dealer.cards = newDealer
-      this.setState({})
-      this.playStand()
-      
-    } else {
-     
-      if (this.state.player.count > this.state.dealer.count || this.state.dealer.count > 21) {
-        this.win()
-      } else if (this.state.player.count === this.state.dealer.count){ 
-        this.draw()
-      }else {
-        this.lose()
-      }
-    }
-    
-  }
+ 
 
 
   bet5(){
@@ -354,7 +394,7 @@ class App extends React.Component {
       <div className="App">
       <button onClick = {this.setUp}>New Game</button>
       <button onClick = {this.playerHit}>Hit</button>
-      <button onClick = {this.playStand}>Stand</button>
+      <button onClick = {this.dealerHit}>Stand</button>
       <button onClick = {this.double}>Double</button>
        <button onClick = {this.start} style = {{display:this.state.button}}>Bet</button>
        <form>
@@ -372,18 +412,18 @@ class App extends React.Component {
         <div>
         Current Bet: ${this.state.bet}
         </div>
-      <div class="main">
+      <div className="main">
       {this.state.message}
-        <div class="table">
+        <div className="table">
           
           
-          <div class="boardMe" style = {{display:this.state.hand}}>
-          <div class = "words">YOUR HAND {"     "} {this.state.player.count}
+          <div className="boardMe" style = {{display:this.state.hand}}>
+          <div className = "words">YOUR HAND {"     "} {this.state.player.count}
           </div>
               {this.state.player.cards.map((card)=>{
-                return <div class="card-small">
-                <p class="card-text">{card.number}</p>
-                <p class="card-img ">{card.suit}</p>
+                return <div className="card-small">
+                <p className="card-text">{card.number}</p>
+                <p className="card-img ">{card.suit}</p>
               </div>
 
               })}
@@ -396,13 +436,13 @@ class App extends React.Component {
          
           
           <div className = "boardYou" style = {{display:this.state.hand}}>
-          <div class = "words2">
+          <div className = "words2">
               Dealer's hand 
           </div>
           {this.state.dealer.cards.map((card)=>{
-                return <div class="card-small">
-                <p class="card-text">{card.number}</p>
-                <p class="card-img ">{card.suit}</p>
+                return <div className="card-small">
+                <p className="card-text">{card.number}</p>
+                <p className="card-img ">{card.suit}</p>
               </div>
 
               })}          
